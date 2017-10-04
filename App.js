403ef40e07967a543f18b21847f6439c7529c78c
Luiz Figueiredo <lerfbr@gmail.com>
2017-10-04 09:54:53 -0300

    feat: Steppers & Sliders

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { getMetricMetaInfo } from './utils/helpers'
+import { View } from 'react-native';
+import AddEntry from './components/AddEntry'
 
 export default class App extends React.Component {
   render() {
     return (
       <View>
-        {getMetricMetaInfo('bike').getIcon()}
+        <AddEntry />
       </View>
     );
   }
--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,12 +1,65 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
-import { getMetricMetaInfo } from '../utils/helpers'
+import { getMetricMetaInfo } from './../utils/helpers'
+import UdaciSlider from './UdaciSlider'
+import UdaciSteppers from './UdaciSteppers'
 
 export default class AddEntry extends Component {
+  state = {
+    run: 0,
+    bike: 0,
+    swim: 0,
+    sleep: 0,
+    eat: 0
+  }
+  increment = metric => {
+    const { max, step } = getMetricMetaInfo(metric)
+    this.setState(state => {
+      const count = state[metric] + step
+      return {
+        ...state,
+        [metric]: count > max ? max : count
+      }
+    })
+  }
+  decrement = metric => {
+    this.setState(state => {
+      const count = state[metric] - getMetricMetaInfo(metric).step
+      return {
+        ...state,
+        [metric]: count < 0 ? 0 : count
+      }
+    })
+  }
+  slide = (metric, value) => {
+    this.setState(() => ({
+      [metric]: value
+    }))
+  }
   render() {
+    const metaInfo = getMetricMetaInfo()
     return (
       <View>
+        {Object.keys(metaInfo).map(key => {
+          const { getIcon, type, ...rest } = metaInfo[key]
+          const { value } = this.state[key]
 
+          return (
+            <View key={key}>
+              {getIcon()}
+              {type === 'slider' ?
+                <UdaciSlider
+                  value={value}
+                  onChage={value => this.slide(key, value)}
+                  {...rest}/> :
+                <UdaciSteppers
+                  value={value}
+                  onIcrement={value => this.increment(key)}
+                  onDecrement={value => this.increment(key)}
+                  {...rest} />}
+            </View>
+          )
+        })}
       </View>
     )
   }
